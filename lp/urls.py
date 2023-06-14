409acbcb09ac94a3e51480a1d535f9927a9480e9
Andressa-Anthero7 <andressa.anthero7@gmail.com>
2023-06-14 13:34:37 -0300

    deploy producao 14/06/2023

--- lp/urls.py
+++ lp/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('parque_sao_lucas/', views.landingpage_psl, name='parque-sao-lucas'),
-    path('formulario_parque_sao_lucas/', views.landingpage_form_psl, name='formulario-parque-sao-lucas'),
+    path('inscricao_parque_sao_lucas/', views.landingpage_form_psl, name='formulario-parque-sao-lucas'),
     path('pagina_agradecimento/', views.pagina_agradecimento, name='pagina-agradecimento'),
     path('accounts/login/dashboard/', views.dashboard, name='dashboard'),
 
