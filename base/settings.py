107278cca8cc93cd86a303e272928aa7ff8ea662
Andressa-Anthero7 <andressa.anthero7@gmail.com>
2023-06-14 23:39:09 -0300

    correcao debug,seetings

--- base/settings.py
+++ base/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--6$-ns@pnrs0x(qesbcbl&w#=v@%)+!0aiuwydg8=s#t$b860g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
